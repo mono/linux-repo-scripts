db8124c29cd8b7ef779e1050c03c797a29b434c0
Jo Shields <jo.shields@xamarin.com>
2016-04-21 15:40:03 +0100

    Silence script, for cron use

--- jenkins/purge-old-packages-rpm.sh
+++ jenkins/purge-old-packages-rpm.sh
@@ -8,18 +8,13 @@ pushd ${TOPDIR}
 for lane in `ls -d */* | grep -v repodata`
 	do lastyearmonth=""
 	for package in `ls -1 ${lane}/ | head -n -${NUMKEEP}`
-		do echo "evaluating $package"
-		if [ `echo ${package} | grep -cP '\d{14}'` -gt 0 ]
-		then echo "old date format, ignore"
-		else
-			yearmonth=`echo ${package} | grep -Po '\d{4}\.\d{2}'`
+		do if [ `echo ${package} | grep -cP '\d{14}'` -eq 0 ]
+			then yearmonth=`echo ${package} | grep -Po '\d{4}\.\d{2}'`
 			if [ "${yearmonth}" == "${lastyearmonth}" ]
 			then
-				echo "deleteytime"
 				echo rm -r ${package}
 			else
 				lastyearmonth=${yearmonth}
-				echo "seeing ${lastyearmonth}"
 			fi
 		fi
 	done
