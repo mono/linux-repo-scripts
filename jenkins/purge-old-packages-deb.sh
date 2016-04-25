386c7efa6fbd01c3c019c7b0510b347af46627dc
Jo Shields <jo.shields@xamarin.com>
2016-04-25 11:34:27 +0100

    Valid rm command for debian repo

--- jenkins/purge-old-packages-deb.sh
+++ jenkins/purge-old-packages-deb.sh
@@ -6,15 +6,17 @@
 PACKAGESETS="mono-4.0-snapshot-2 mono-4.2-snapshot-2 mono-4.4-snapshot-2 mono-snapshot-2 monodevelop-snapshot-2"
 # number of packages to keep, beyond 1-a-month rule
 NUMKEEP=20
+# aptly repo to use
+REPONAME=jenkins1
 
 for packageset in ${PACKAGESETS}
 	do lastyearmonth=""
-	for package in $(aptly mirror search -format '{{.Package}}' jenkins1 "Name (% ${packageset}*), \$Architecture (source)" 2> /dev/null| sort | head -n -${NUMKEEP})
+	for package in $(aptly mirror search -format '{{.Package}}' ${REPONAME} "Name (% ${packageset}*), \$Architecture (source)" 2> /dev/null| sort | head -n -${NUMKEEP})
 		do if [ `echo ${package} | grep -cP '\d{14}'` -eq 0 ]
 			then yearmonth=`echo ${package} | grep -Po '\d{4}\.\d{2}'`
 			if [ "${yearmonth}" == "${lastyearmonth}" ]
 			then
-				echo rm -r ${package}
+				echo "aptly repo remove ${REPONAME} "\$Source (${package})\""
 			else
 				lastyearmonth=${yearmonth}
 			fi
