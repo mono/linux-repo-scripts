86ac7511d91e126af9121ee6188664ce8588818d
Jo Shields <jo.shields@xamarin.com>
2016-04-29 15:12:50 +0100

    Fixes

--- jenkins/purge-old-packages-deb.sh
+++ jenkins/purge-old-packages-deb.sh
@@ -7,16 +7,16 @@ PACKAGESETS="mono-4.0-snapshot-2 mono-4.2-snapshot-2 mono-4.4-snapshot-2 mono-sn
 # number of packages to keep, beyond 1-a-month rule
 NUMKEEP=20
 # aptly repo to use
-REPONAME=jenkins1
+REPONAME="sid"
 
 for packageset in ${PACKAGESETS}
 	do lastyearmonth=""
-	for package in $(aptly mirror search -format '{{.Package}}' ${REPONAME} "Name (% ${packageset}*), \$Architecture (source)" 2> /dev/null| sort | head -n -${NUMKEEP})
+	for package in $(aptly repo search -format '{{.Package}}' ${REPONAME} "Name (% ${packageset}*), \$Architecture (source)" 2> /dev/null| sort | head -n -${NUMKEEP})
 		do if [ `echo ${package} | grep -cP '\d{14}'` -eq 0 ]
 			then yearmonth=`echo ${package} | grep -Po '\d{4}\.\d{2}'`
 			if [ "${yearmonth}" == "${lastyearmonth}" ]
 			then
-				echo "aptly repo remove ${REPONAME} "\$Source (${package})\""
+				echo "aptly repo remove ${REPONAME} \"\$Source (${package})\""
 			else
 				lastyearmonth=${yearmonth}
 			fi
